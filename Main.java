a4444a99e93b22de9ee4a27daa9d53722929f1cf
Mateo Correa Risso <90853350+mateocorrea99@users.noreply.github.com>
2022-12-03 06:05:43 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Akari;
+
+import Akari.view.AppLauncher;
+import javafx.application.Application;
+
+public class Main {
+  public static void main(String[] args) {
+    Application.launch(AppLauncher.class);
+  }
+}
